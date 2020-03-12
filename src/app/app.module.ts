90bcb8f9881d8b2d0c45fe832a4257a95ed72109
fardo9 <nazz.fardo@gmail.com>
2020-03-12 22:49:18 +0200

    add header and foooter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+// import { FooterComponent } from './component/footer/footer.component';
+// import { MainBottomDropmenuComponent } from './component/partials-menu/main-bottom-dropmenu/main-bottom-dropmenu.component';
+// import { SwiperSlideComponent } from './component/swiper-slide/swiper-slide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    // FooterComponent,
+    // MainBottomDropmenuComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
